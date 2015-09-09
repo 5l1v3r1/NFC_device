7983980992282c453dcc5f251dcb70e43f24b169
Ratnesh Zinzuwadia <ratnesh_zin@yahoo.com>
2015-09-09 14:43:30 +0530

    make_project.sh and make_clean.sh made smart

--- build/make_clean.sh
+++ build/make_clean.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-rm -r CMakeFiles/ CMakeCache.txt
-rm Makefile
-rm nfc
-rm cmake_install.cmake
-rm .cproject .project
+#Add file that want to remove on this script execution
+array=("CMakeFiles/" "CMakeCache.txt" "Makefile" "nfc" "cmake_install.cmake" ".cproject" ".project")
+
+i=0
+while [ $i != ${#array[@]} ]; do
+	if [ -e  ${array[$i]} ]; then
+		rm -r ${array[$i]}
+		echo "removed ${array[$i]}"
+	fi
+	i=`expr $i + 1`
+done
--- build/make_project.sh
+++ build/make_project.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-# Generate project steps...
+# Project generation steps...
 # ./make_project.sh CDT 	= for eclipse file generated
 # ./make_project.sh CDT ARM = for eclipse file generated with Cross Compile for arm
 # ./make_project.sh ARM 	= Only Cross Compile for arm
+# make to compile project
 
 # Comment it for Release version build
 DEBUG="DEBUG"
 
 FIRST_PARA="$1"
 SECOND_PARA="$2"
+COMMAND="cmake"
 
 #$@ for printing all argument
 #$# for numbers of passed argument
 echo "$# Arguments : $@"
 
-if [ $# -ge '1' ] && [ $FIRST_PARA == 'CDT' ]; then
-
-	echo "Make project with CDT..."
-	if  [ $# == '2' ] && [ $SECOND_PARA == 'ARM' ]; then
-		echo "Cross compiling..."
-		cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=rpi_toolchain.cmake -D CMAKE_BUILD_TYPE=$DEBUG -lpthread .
-	else
-		cmake -G "Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=$DEBUG -lpthread .
-	fi
-
-else
-
-	echo "Make project without CDT..."
-	if [ $# -eq '1' ] && [ $FIRST_PARA == 'ARM' ]; then
-		echo "Cross compiling..."
-		cmake -DCMAKE_TOOLCHAIN_FILE=rpi_toolchain.cmake -D CMAKE_BUILD_TYPE=$Debug -lpthread .
-	else
-		cmake -D CMAKE_BUILD_TYPE=$Debug -lpthread .
-	fi
-
+COMMAND+=" -D CMAKE_BUILD_TYPE=$DEBUG -lpthread . "
+if [[ $@ == *"CDT"* ]]; then
+	echo "With Eclipse project."
+	COMMAND+="-G \"Eclipse CDT4 - Unix Makefiles\""
 fi
+
+if [[ $@ == *"ARM"* ]]; then
+	echo "ARM define for Cross Compile."
+	COMMAND+=" -DCMAKE_TOOLCHAIN_FILE=rpi_toolchain.cmake "
+fi
+echo "Execute Command : $COMMAND"
+
+eval $COMMAND
+echo "************************************************"
